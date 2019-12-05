80f27927c28422d3eece4a81249ec586d6d266a5
Maxim <starlock2015@gmail.com>
2019-12-05 18:17:16 +0200

    CALCULATOR1.2 метод предлагающий повторно ввести пример при выявлении ошибок

--- src/Task2/SimpleCalculator.java
+++ src/Task2/SimpleCalculator.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
     public static void main(String[] args) {
+        calculation();
+    }
+
+
+    //вся вычислительная логика выделена в отдельный метод для возможности повторного самовызова через ссылку на название метода
+    private static void calculation(){
 
         //объявление переменных, которым присвоены данные, введенные пользователям
         double first = inputFirst();
         String action = inputAction();
         double second = inputSecond();
-
 
         //блок содержащий вычислительную логику на основе оператора if-else-if и выводящий результат на экран
         if(action.equals("-")){
@@ -24,12 +29,26 @@ public class SimpleCalculator {
                 System.out.println("Результат: " + first / second);
             }else{
                 System.out.println("Делить на ноль нельзя!!!");
+                repeat();//
             }
         }else{
-            System.out.println("Неизвестная математическая операция");
+            System.out.println("Неизвестная математическая операция.");
+            repeat();
         }
-
     }
+
+    //метод вызывается при багах и спрашивает хочет ли пользователь повторить ввод примера нажатием на Y
+    private static void repeat() {
+        System.out.println("Желаете повторить ввод примера? y/n?");
+        Scanner scanner = new Scanner(System.in);
+        String restart =  scanner.next();
+
+        //благодаря логическому или функция сработает дваже при капслоке или русскоязычной раскладке
+        if (restart.equals("y")||restart.equals("Y")||restart.equals("н")||restart.equals("Н")){
+            calculation();
+        }
+    }
+
 
     //метод возвращает число  введенное пользователем
     private static double inputFirst(){
@@ -51,4 +70,6 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
         return scanner.next();
     }
+
+
 }
