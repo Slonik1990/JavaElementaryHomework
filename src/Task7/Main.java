4dffcd7f3a209757815404e20895d033df567d17
Maxim <starlock2015@gmail.com>
2020-01-08 02:46:58 +0200

    аве Java

--- src/Task7/Main.java
+++ src/Task7/Main.java
@@ -21,10 +21,17 @@ public class Main {
             zoo[i].say();
         }
 
+
+        for (int i = 0; i < zoo.length; i++) {
+            zoo[i].food();
+        }
+
         System.out.println();
         for (int i = 0; i < zoo.length; i++) {
             System.out.println(zoo[i].getId());
         }
+
+
 
         chappi.goHome();//Sorry I am not trained dog, but next time I will be ready!!!
         chappi.goHome();//Hello!!! My name is Chappi. Woof!!!I can take you home. Let's go!!!
