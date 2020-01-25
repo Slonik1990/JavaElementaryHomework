dffe3c9587954d203f5590f195fb10e8de7001be
Maxim <starlock2015@gmail.com>
2020-01-26 01:08:30 +0200

    аве Java

--- src/Task12/Main.java
+++ src/Task12/Main.java
@@ -11,28 +11,30 @@ public class Main {
         IntTreeSet my = new IntTreeSet();
 
 
-            my.add(4);
+
         my.add(50);
         my.add(30);
-        my.add(70);
-        my.add(10);
-        my.add(40);
-        my.add(60);
-        my.add(80);
+//        my.add(70);
+//        my.add(10);
+//        my.add(40);
+//        my.add(60);
+//        my.add(80);
+//
+//        my.add(1000);
 
-        my.add(1000);
-
-        System.out.println(my);
+        System.out.println(my);//TreeSet content: [30] [50]
         System.out.println();
         Iterator iter = my.iterator();
 
-        System.out.println(iter.hasNext());
-        System.out.println(iter.hasNext());
-        System.out.println(iter.hasNext());
+        System.out.println(iter.hasNext());//true
+        System.out.println(iter.next());//30
+        System.out.println(iter.hasNext());//true
+        System.out.println(iter.next());//50
+        System.out.println(iter.hasNext());//false
 
         for (Object i: my){
 
-            System.out.println(i);
+            System.out.print (i + " ");//30 50
 
         }
 
