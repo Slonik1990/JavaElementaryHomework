64e982223290cfbcc1f231f286f0196774925396
Maxim <starlock2015@gmail.com>
2020-04-01 08:43:52 +0300

    !

--- src/Task23Hacker/Main.java
+++ src/Task23Hacker/Main.java
@@ -1,47 +1,63 @@
 package Task23Hacker;
 
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 //f016441d00c16c9b912d05e9d81d894d - very
 //5ebe2294ecd0e0f08eab7690d2a6ee69 - secret
-//13d70e09909669272b19647c2a55dacb
+//13d70e09909669272b19647c2a55dacb - goodforyou
 //5f50dfa5385e66ce46ad8d08a9c9be68
+
+//4bea249142664d11a289ffdf30225a91
+
 
 public class Main {
     public static void main(String[] args) {
 
-        Thread myThread1 = new Thread("a"){
-            @Override
-            public void run() {
-                long l = System.currentTimeMillis();
-                MyMD5 hak = new MyMD5("abcdefghijklmnopqrstuvwxyz");
-                System.out.println("Распознано слово: " + hak.decoderMD5("5ebe2294ecd0e0f08eab7690d2a6ee69"));
-                System.out.println(System.currentTimeMillis() - l);
-                System.out.println(getName());
-                System.exit(0);
-            }
-        };
+        String symbols = "abcdefghijklmnopqrstuvwxyz"; // строка содержащая все символы участвующие в переборе
+        String hash = "5ebe2294ecd0e0f08eab7690d2a6ee69"; // хэшированный пароль
+        MyMD5 myMD51 = new MyMD5(symbols, 1, 7, hash);
+        MyMD5 myMD52 = new MyMD5(symbols, 7, 13, hash);
+        MyMD5 myMD53 = new MyMD5(symbols, 14, 20, hash);
+        MyMD5 myMD54 = new MyMD5(symbols, 21, 26, hash);
 
-        Thread myThread2 = new Thread("b"){
-            @Override
-            public void run() {
-                long l = System.currentTimeMillis();
-                MyMD5 hak = new MyMD5("zyxwvutsrqponmlkjihgfedcba");
-                System.out.println("Распознано слово: " + hak.decoderMD5("5ebe2294ecd0e0f08eab7690d2a6ee69"));
-                System.out.println(System.currentTimeMillis() - l);
-                System.out.println(getName());
-                System.exit(0);
-            }
-        };
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+
+        executorService.submit(myMD51);
+        executorService.submit(myMD52);
+        executorService.submit(myMD53);
+        executorService.submit(myMD54);
 
 
 
 
+//        Thread thread1 = new Thread(){
+//            @Override
+//            public void run() {
+//                myMD5.hacker(hash, 1,1);
+//            }
+//        };
+//        Thread thread2 = new Thread(){
+//            @Override
+//            public void run() {
+//                myMD5.hacker(hash, 2,2);
+//            }
+//        };
+//
+//
+//        thread1.start();
+//        thread2.start();
 
 
-        myThread1.start();
-        myThread2.start();
 
 
     }
--- src/Task23Hacker/MyMD5.java
+++ src/Task23Hacker/MyMD5.java
@@ -1,82 +1,212 @@
 package Task23Hacker;
 
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.w3c.dom.ls.LSOutput;
+
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+
 import java.security.NoSuchAlgorithmException;
 
-public class MyMD5 {
-    private char[] symbols = new String("abcdefghijklmnopqrstuvwxyz").toCharArray();
+public class MyMD5 implements Runnable{
+    private char[] symbols;
+    private int from;
+    private int to;
+    private String hash;
 
-    public MyMD5() {
-    }
 
-    public MyMD5(String symbols) {
+
+    public MyMD5(String symbols, int from, int to, String hash) {
         this.symbols = symbols.toCharArray();
+        this.from = from;
+        this.to = to;
+        this.hash = hash;
     }
 
-    public String decoderMD5(String hash) {
-//        генерация слова
-        for (int i = 0; i < symbols.length; i++) {
+    @Override
+    public void run() {
+        hacker();
+    }
+
+
+    public void hacker (){
+        long start = System.currentTimeMillis();
+        boolean status;
+
+        status = decoder4(hash, from, to);
+        if(!status){
+            status = decoder5(hash, from, to);
+        }
+        if(!status){
+            status = decoder6(hash, from, to);
+        }
+        if(!status){
+            status = decoder7(hash, from, to);
+        }
+        if(!status){
+            status = decoder8(hash, from, to);
+        }
+        System.out.println(Thread.currentThread().getName() +" finished. TOTAL TIME " + (System.currentTimeMillis() - start));
+        System.exit(0);
+    }
+
+    public boolean decoder4(String hash, int from, int to) {
+        long start = System.currentTimeMillis();
+        for (int i = from - 1; i < to; i++) {
+            for (int j = 0; j < symbols.length; j++) {
+                for (int k = 0; k < symbols.length; k++) {
+                    for (int l = 0; l < symbols.length; l++) {
+
+                        char[] letters = {symbols[i], symbols[j], symbols[k], symbols[l]};
+                        String word = new String(letters);
+                        String wordHash = coderApache(word);
+                        if (wordHash.equals(hash)) {
+                            System.out.println("DECRYPTED WORD: " + word);
+                            return true;
+                        }
+
+                    }
+                }
+            }
+        }
+        System.out.println("4-character combinations finished - time: " + (System.currentTimeMillis() - start));
+        return false;
+    }
+
+    public boolean decoder5(String hash, int from, int to) {
+        long start = System.currentTimeMillis();
+        for (int i = from - 1; i < to; i++) {
+            for (int j = 0; j < symbols.length; j++) {
+                for (int k = 0; k < symbols.length; k++) {
+                    for (int l = 0; l < symbols.length; l++) {
+                        for (int m = 0; m < symbols.length; m++) {
+
+                            char[] letters = {symbols[i], symbols[j], symbols[k], symbols[l], symbols[m]};
+                            String word = new String(letters);
+                            String wordHash = coderApache(word);
+                            if (wordHash.equals(hash)) {
+                                System.out.println("DECRYPTED WORD: " + word);
+                                return true;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("5-character combinations finished - time: " + (System.currentTimeMillis() - start));
+        return false;
+    }
+
+    public boolean decoder6(String hash, int from, int to) {
+        long start = System.currentTimeMillis();
+        for (int i = from - 1; i < to; i++) {
             for (int j = 0; j < symbols.length; j++) {
                 for (int k = 0; k < symbols.length; k++) {
                     for (int l = 0; l < symbols.length; l++) {
                         for (int m = 0; m < symbols.length; m++) {
                             for (int n = 0; n < symbols.length; n++) {
-
-
-                                if (i == 0 & j == 0) {
-                                    char[] letters = {symbols[k], symbols[l], symbols[m], symbols[n]};
-                                    String word = new String(letters);
-                                    //сравнение хэш кода
-                                    String wordHash = coderMD5(word);
-                                    if (wordHash.equals(hash)) {
-                                        return word;
-                                    }
-                                }
-
-                                if (i == 0) {
-                                    char[] letters = {symbols[j], symbols[k], symbols[l], symbols[m], symbols[n]};
-                                    String word = new String(letters);
-                                    //сравнение хэш кода
-                                    String wordHash = coderMD5(word);
-                                    if (wordHash.equals(hash)) {
-                                        return word;
-                                    }
-                                }
-
                                 char[] letters = {symbols[i], symbols[j], symbols[k], symbols[l], symbols[m], symbols[n]};
                                 String word = new String(letters);
-                                //сравнение хэш кода
-                                String wordHash = coderMD5(word);
+                                String wordHash = coderApache(word);
                                 if (wordHash.equals(hash)) {
-                                    return word;
+                                    System.out.println("DECRYPTED WORD: " + word);
+                                    return true;
                                 }
-
                             }
                         }
                     }
                 }
             }
         }
-        System.out.println("не нашел");
+        System.out.println("6-character combinations finished - time: " + (System.currentTimeMillis() - start));
+        return false;
+    }
+
+    public boolean decoder7(String hash, int from, int to) {
+        long start = System.currentTimeMillis();
+        for (int i = from - 1; i < to; i++) {
+            for (int j = 0; j < symbols.length; j++) {
+                for (int k = 0; k < symbols.length; k++) {
+                    for (int l = 0; l < symbols.length; l++) {
+                        for (int m = 0; m < symbols.length; m++) {
+                            for (int n = 0; n < symbols.length; n++) {
+                                for (int o = 0; o < symbols.length; o++) {
+                                    char[] letters = {symbols[i], symbols[j], symbols[k], symbols[l], symbols[m], symbols[n], symbols[n]};
+                                    String word = new String(letters);
+                                    String wordHash = coderApache(word);
+                                    if (wordHash.equals(hash)) {
+                                        System.out.println("DECRYPTED WORD: " + word);
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("7-character combinations finished - time: " + (System.currentTimeMillis() - start));
+        return false;
+    }
+
+    public boolean decoder8(String hash, int from, int to) {
+        long start = System.currentTimeMillis();
+        for (int i = from - 1; i < to; i++) {
+            for (int j = 0; j < symbols.length; j++) {
+                for (int k = 0; k < symbols.length; k++) {
+                    for (int l = 0; l < symbols.length; l++) {
+                        for (int m = 0; m < symbols.length; m++) {
+                            for (int n = 0; n < symbols.length; n++) {
+                                for (int o = 0; o < symbols.length; o++) {
+                                    for (int p = 0; p < symbols.length; p++) {
+                                        char[] letters = {symbols[i], symbols[j], symbols[k], symbols[l], symbols[m], symbols[n], symbols[o], symbols[p]};
+                                        String word = new String(letters);
+                                        String wordHash = coderApache(word);
+                                        if (wordHash.equals(hash)) {
+                                            System.out.println("DECRYPTED WORD: " + word);
+                                            return true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("8-character combinations finished - time: " + (System.currentTimeMillis() - start));
+        return false;
+    }
+
+
+    //различные реализации кодировщиков
+    private static String coderApache(String text) {
+        return DigestUtils.md5Hex(text);
+    }
+
+    //одна из реализаций статическая, для использования кодировщика как утилиты для пользователя
+    public static String getHashCode(String text) {
+        HashFunction hashFunction = Hashing.md5();
+        HashCode hash = hashFunction.hashString(text, StandardCharsets.UTF_8);
+        return hash.toString();
+    }
+
+    public String coderDataConvertor(String text) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = md5.digest(text.getBytes());
+            return DatatypeConverter.printHexBinary(bytes).toLowerCase();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
 
-    public String coderMD5(String text) {
-        StringBuilder sb = new StringBuilder();
-
-        try {
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            byte[] bytes = md5.digest(text.getBytes());
-            for (Byte b : bytes) {
-                sb.append(String.format("%02x", b));
-            }
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-
-    }
 
 }
