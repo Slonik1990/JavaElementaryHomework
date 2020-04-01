24d64fb1cad76c6ca1be5a208dab8207640eeaed
Maxim <starlock2015@gmail.com>
2020-04-01 08:51:55 +0300

    !

--- src/Task23Hacker/Main.java
+++ src/Task23Hacker/Main.java
@@ -15,18 +15,17 @@ import java.util.concurrent.Executors;
 //13d70e09909669272b19647c2a55dacb - goodforyou
 //5f50dfa5385e66ce46ad8d08a9c9be68
 
-//4bea249142664d11a289ffdf30225a91
 
 
 public class Main {
     public static void main(String[] args) {
 
-        String symbols = "abcdefghijklmnopqrstuvwxyz"; // строка содержащая все символы участвующие в переборе
-        String hash = "5ebe2294ecd0e0f08eab7690d2a6ee69"; // хэшированный пароль
-        MyMD5 myMD51 = new MyMD5(symbols, 1, 7, hash);
-        MyMD5 myMD52 = new MyMD5(symbols, 7, 13, hash);
-        MyMD5 myMD53 = new MyMD5(symbols, 14, 20, hash);
-        MyMD5 myMD54 = new MyMD5(symbols, 21, 26, hash);
+        String symbols = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_-+="; // строка содержащая все символы участвующие в переборе
+        String hash = "5f50dfa5385e66ce46ad8d08a9c9be68"; // хэшированный пароль
+        MyMD5 myMD51 = new MyMD5(symbols, 1, 10, hash);
+        MyMD5 myMD52 = new MyMD5(symbols, 11, 20, hash);
+        MyMD5 myMD53 = new MyMD5(symbols, 21, 30, hash);
+        MyMD5 myMD54 = new MyMD5(symbols, 31, 40, hash);
 
 
 
@@ -40,22 +39,7 @@ public class Main {
 
 
 
-//        Thread thread1 = new Thread(){
-//            @Override
-//            public void run() {
-//                myMD5.hacker(hash, 1,1);
-//            }
-//        };
-//        Thread thread2 = new Thread(){
-//            @Override
-//            public void run() {
-//                myMD5.hacker(hash, 2,2);
-//            }
-//        };
-//
-//
-//        thread1.start();
-//        thread2.start();
+
 
 
 
